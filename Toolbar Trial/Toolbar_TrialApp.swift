b77e515524330141221a374d00f527ba53800494
sourav-bz <s.kumar001997@gmail.com>
2025-02-18 18:57:50 +0530

    Initial Commit

---
+++ Toolbar Trial/Toolbar_TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toolbar_TrialApp.swift
+//  Toolbar Trial
+//
+//  Created by Sourav on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Toolbar_TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Toolbar TrialTests/Toolbar_TrialTests.swift
@@ -0,0 +1,17 @@
+//
+//  Toolbar_TrialTests.swift
+//  Toolbar TrialTests
+//
+//  Created by Sourav on 18/02/25.
+//
+
+import Testing
+@testable import Toolbar_Trial
+
+struct Toolbar_TrialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
